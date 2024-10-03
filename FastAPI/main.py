97b29eb57ab6d5d9635945cb54dd26faf7a38355
Erik Gabriel <126505086+erikgabriel07@users.noreply.github.com>
2024-10-03 10:27:04 -0300

    Add files via upload

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -12,39 +12,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# Alteração feita abaixo por Valter Junior
-
-from fastapi import FastAPI, HTTPException
-import os
-
-app = FastAPI()
-
-# Defina o diretório onde os arquivos estão localizados
-DIRETORIO_ARQUIVOS = "./arquivos"
-
-
-@app.get("/arquivos/")
-async def listar_arquivos():
-    try:
-        # Listar arquivos no diretório especificado
-        arquivos = os.listdir(DIRETORIO_ARQUIVOS)
-        # Filtrar apenas arquivos (ignorando subdiretórios)
-        arquivos = [f for f in arquivos if os.path.isfile(os.path.join(DIRETORIO_ARQUIVOS, f))]
-        return {"arquivos": arquivos}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.post("/processar/")
-async def processar_arquivos(arquivo: str):
-    # Verifique se o arquivo existe
-    caminho_arquivo = os.path.join(DIRETORIO_ARQUIVOS, arquivo)
-    if not os.path.isfile(caminho_arquivo):
-        raise HTTPException(status_code=404, detail="Arquivo não encontrado")
-
-    # Aqui você pode aplicar a lógica de processamento que deseja
-    # Por exemplo, vamos apenas retornar o tamanho do arquivo
-    tamanho = os.path.getsize(caminho_arquivo)
-
-    return {"arquivo": arquivo, "tamanho": tamanho}
