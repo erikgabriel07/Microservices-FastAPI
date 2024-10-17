bf6da173271d92673638443fdae75958cab7282c
josev <jrdiasmkt@gmail.com>
2024-10-17 14:33:48 -0300

    router.get
    Listar_Arquivos

--- FastAPI/routes/file_routes.py
+++ FastAPI/routes/file_routes.py
@@ -10,3 +10,9 @@ router = APIRouter()
              description="Este endpoint recebe um arquivo CSV e retorna uma mensagem de confirmação.")
 async def enviar_dados(file: UploadFile = File(...)):
     return await FileProcessor().upload_file(file)
+
+@router.get("/file/listar_arquivos",
+            summary="Listar Arquivos Enviados",
+            description="Este endpoint retorna uma lista dos arquivos enviados.")
+async def listar_arquivos():
+    return await FileProcessor().list_files()
