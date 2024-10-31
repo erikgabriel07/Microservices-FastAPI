eb2724c4812a37252a5c88374133e6082c67b174
Erik Gabriel <126505086+erikgabriel07@users.noreply.github.com>
2024-10-31 19:55:31 -0300

    Update file_routes.py

--- FastAPI/routes/file_routes.py
+++ FastAPI/routes/file_routes.py
@@ -30,25 +30,3 @@ async def upload_tributo_competencia(
     file: UploadFile = File(..., description='Arquivo a ser carregado.')
 ):
     return await FileProcessor().upload_file(file)
-
-@router.patch("/file/delete/",
-            summary="Remoção de dados",
-            description="Essa rota remove dados do banco de dados")
-@verify_id_parameter
-async def delete(
-    id: int = Query(None, alias='Identifier', ge=0,
-                    description='ID do item a ser removido.'),
-):
-    return {'message': 'Data deleted successfully!'}
-
-@router.patch("/file/duplicate/",
-            summary="Define se um dado é duplicado",
-            description="Essa rota marca um dado no banco de dados como duplicado")
-@verify_id_parameter
-async def duplicate(
-    id: int = Query(None, alias='Identifier', ge=0,
-                    description='ID do item a ser marcado como duplicado.'),
-    value: bool = Query(False, alias='Value',
-                        description='Valor a ser configurado.')
-):
-    return {'message': 'Data \'duplicate\' was updated successfully!'}
